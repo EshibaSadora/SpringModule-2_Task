8ffc3b62a93178351db5e8d053c7072d696fd9ab
EshibaSadora <egorshornikov@mail.ru>
2022-08-30 12:34:24 +0300

    CreateProject

---
+++ src/main/java/ru/shornikov/ShornikovApplication.java
@@ -0,0 +1,13 @@
+package ru.shornikov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShornikovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShornikovApplication.class, args);
+	}
+
+}
